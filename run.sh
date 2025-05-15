0931c9f51481449b69fa4b031940bb47607c2ed4
Santiago <srivas@itba.edu.ar>
2025-05-15 14:52:13 -0300

    create run

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+python src/rag_refactored.py index --dossier_dir data/space_data/dossiers --html_dir data/html --event_id 1 --reset_db --chunk_size 2048 --chunk_overlap 128
+
+python src/rag_refactored.py suggest --user_id celebrity-0001 celebrity-0002 celebrity-0003 celebrity-0004 --event_id 1 --evaluate --chunk_size 2048 --chunk_overlap 128
+
